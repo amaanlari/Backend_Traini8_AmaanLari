36f35c286a71a735e74db40893b67e22ac06eaf5
amaanlari <amaanlari@outlook.comm>
2025-03-19 12:47:13 +0530

    feat: add global exception handling for validation errors in API

--- src/main/java/com/lari/trainingcenter/advice/GlobalExceptionHandler.java
+++ src/main/java/com/lari/trainingcenter/advice/GlobalExceptionHandler.java
@@ -2,12 +2,18 @@ package com.lari.trainingcenter.advice;
 
 
 import com.lari.trainingcenter.exceptions.ResourceNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.List;
+
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
@@ -18,6 +24,23 @@ public class GlobalExceptionHandler {
                 .build();
         return buildErrorResponseEntity(apiError);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<?>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        List<String> errors = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .toList();
+
+        ApiError apiError = ApiError.builder()
+                .status(HttpStatus.BAD_REQUEST)
+                .message("Validation error: MethodArgumentNotValidException")
+                .subErrors(errors)
+                .build();
+        return buildErrorResponseEntity(apiError);
+    }
+
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<?>> handleException(Exception e) {
